8a03ea1b018fde2799ff18d9647c6e5a2be3098d
Joe Fuentes <u.j.fuentes@gmail.com>
2020-01-02 23:36:46 -0500

    fix stuff

--- packer_templates/ubuntu/scripts/post-update-installs.sh
+++ packer_templates/ubuntu/scripts/post-update-installs.sh
@@ -6,9 +6,17 @@ major_version="`echo $ubuntu_version | awk -F. '{print $1}'`";
 
 
 # Install apache
-apt-get install apache2
+apt-get install -y apache2
 ufw allow 'Apache Full'
-apt-get install libapache2-modsecurity
+apt-get install -y libapache2-modsecurity
 mv /etc/modsecurity/modsecurity.conf-recommended /etc/modsecurity/modsecurity.conf
+
+# Install website
+cd /var/www/html
+git clone https://github.com/Joey-Fuentes/Joe_Fuentes-Website.git
+rm index.html
+mv Joe_Fuentes-Website/* . || true
+mv Joe_Fuentes-Website/.* . || true
+rm -rf Joe_Fuentes-Website
 
 reboot
